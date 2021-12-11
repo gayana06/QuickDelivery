2869f596e0cbb4fee2a07e818b1e1cbdaab7ea3d
Gayana Chandrasekara <gayana.chandrasekara@qliro.com>
2021-12-11 17:18:30 +0100

    Added description to request

--- QuickDelivery/QuickDelivery/Models/AdminApi/AddProductsRequest.cs
+++ QuickDelivery/QuickDelivery/Models/AdminApi/AddProductsRequest.cs
@@ -7,23 +7,44 @@ namespace QuickDelivery.Models.AdminApi
 {
     public class AddProductsRequest
     {
+        /// <summary>
+        /// List of products
+        /// </summary>
+        /// <example></example>
         [Required]
         [EnsureMinimumElements(1)]
         public List<Product> Products { get; set; }
 
         public class Product
         {
+            /// <summary>
+            /// a string of max length of 200
+            /// </summary>
+            /// <example>productA</example>
             [Required]
+            [StringLength(200)]
             public string Name { get; set; }
 
+            /// <summary>
+            /// List of delivery days
+            /// </summary>
+            /// <example>Monday, Tuesday</example>
             [Required]
             [ValidateEnumStringValueExists(typeof(WeekDay))]
             public List<string> DeliveryDays { get; set; }
 
+            /// <summary>
+            /// List of delivery days
+            /// </summary>
+            /// <example>Normal, External, Temporary</example>
             [Required]
             [ValidateEnumStringValueExists(typeof(ProductType))]
             public string ProductType { get; set; }
 
+            /// <summary>
+            /// Positive number or zero
+            /// </summary>
+            /// <example>3</example>
             [Required]
             [ExternalProductShouldOrder5DaysInAdvance("ProductType")]
             [Range(0, int.MaxValue)]
--- QuickDelivery/QuickDelivery/Models/CustomerApi/GetDeliveryDatesRequest.cs
+++ QuickDelivery/QuickDelivery/Models/CustomerApi/GetDeliveryDatesRequest.cs
@@ -6,10 +6,18 @@ namespace QuickDelivery.Models.CustomerApi
 {
     public class GetDeliveryDatesRequest
     {
+        /// <summary>
+        /// postal code of 5 characters
+        /// </summary>
+        /// <example>12344</example>
         [Required]
         [StringLength(5)]
         public string PostalCode { get; set; }
 
+        /// <summary>
+        /// list of product ids
+        /// </summary>
+        /// <example>1, 2</example>
         [Required]
         [EnsureMinimumElements(1)]
         public List<long> ProductIds { get; set; }
